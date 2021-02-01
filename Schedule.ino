0e7136db717367ee4b75019cb8f1abb7a154f096
Methsara Perera <71716579+methsaramperera@users.noreply.github.com>
2021-02-01 22:13:28 +0530

    Add files via upload

---
+++ Schedule.ino
@@ -0,0 +1,22 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "hMFR0dAOv2ie6jnrfWKsPI3ganlzYH1B";       // You should get Auth Token in the Blynk App.
+const int out = 5;                      // Relay 1 of BlynkBoard Conneted with D1(GPIO 05) pin of NodeMCU or Wemos D1
+char ssid[] = "Your SSID";    // Your WiFi credentials.
+char pass[] = "Password";        // Set password to "" for open networks.
+
+void setup()
+  {
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(out,OUTPUT);
+  }
+  
+void loop()
+  {
+  Blynk.run();
+  }
