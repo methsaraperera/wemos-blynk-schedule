e4244e73fde9311980cf309ed4d972a102974a7a
Methsara Perera <71716579+methsaramperera@users.noreply.github.com>
2021-02-05 22:35:30 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-char auth[] = "hMFR0dAOv2ie6jnrfWKsPI3ganlzYH1B";       // You should get Auth Token in the Blynk App.
+char auth[] = "Your Auth Token";       // You should get Auth Token in the Blynk App.
 const int out = 5;                      // Relay 1 of BlynkBoard Conneted with D1(GPIO 05) pin of NodeMCU or Wemos D1
 char ssid[] = "Your SSID";    // Your WiFi credentials.
 char pass[] = "Password";        // Set password to "" for open networks.
